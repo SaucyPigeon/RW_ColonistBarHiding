11b80dcf81a0c32bf0218d9aaa44dbea0b42dae1
SaucyPigeon <36158286+SaucyPigeon@users.noreply.github.com>
2020-01-20 21:50:23 +0000

    Removed unnecessary FindBestScale() method
    No longer needed in patch class; the new method is found in ColonistBarDrawLocsUtility.

--- Source/ColonistBarHiding/ColonistBarHiding/Patches/ColonistBarDrawLocsFinder_FindBestScale.cs
+++ Source/ColonistBarHiding/ColonistBarHiding/Patches/ColonistBarDrawLocsFinder_FindBestScale.cs
@@ -18,57 +18,6 @@ namespace ColonistBarHiding.Patches
 	[HarmonyPatch("FindBestScale")]
 	internal class ColonistBarDrawLocsFinder_FindBestScale
 	{
-		// Modified private method ColonistBarDrawLocsFinder.FindBestScale().
-		private static float FindBestScale(out bool onlyOneRow, out int maxPerGlobalRow, ref List<int> entriesInGroup, ref List<int> horizontalSlotsPerGroup)
-		{
-			float bestScale = 1f;
-			List<ColonistBar.Entry> entries = Find.ColonistBar.Entries;
-			int groupsCount = ColonistBarUtility.CalculateGroupsCount();
-			while (true)
-			{
-				float num3 = (ColonistBar.BaseSize.x + 24f) * bestScale;
-				float num4 = ColonistBarDrawLocsUtility.MaxColonistBarWidth - (float)(groupsCount - 1) * 25f * bestScale;
-				maxPerGlobalRow = Mathf.FloorToInt(num4 / num3);
-				onlyOneRow = true;
-				bool result = ColonistBarDrawLocsUtility.TryDistributeHorizontalSlotsBetweenGroups(
-					maxPerGlobalRow, entriesInGroup, out horizontalSlotsPerGroup);
-				if (result)
-				{
-					int allowedRowsCountForScale = ColonistBarDrawLocsUtility.GetAllowedRowsCountForScale(bestScale);
-					bool flag = true;
-					int num5 = -1;
-
-					foreach (var entry in entries)
-					{
-						if (!ColonistBarUtility.IsHidden(entry))
-						{
-							if (num5 != entry.group)
-							{
-								num5 = entry.group;
-								int num6 = Mathf.CeilToInt((float)entriesInGroup[entry.group] / (float)horizontalSlotsPerGroup[entry.group]);
-								if (num6 > 1)
-								{
-									onlyOneRow = false;
-								}
-								if (num6 > allowedRowsCountForScale)
-								{
-									flag = false;
-									break;
-								}
-							}
-						}
-					}
-
-					if (flag)
-					{
-						break;
-					}
-				}
-				bestScale *= 0.95f;
-			}
-			return bestScale;
-		}
-
 		[HarmonyPrefix]
 		private static bool Prefix(float __result, out bool onlyOneRow, out int maxPerGlobalRow, ref List<int> ___entriesInGroup, ref List<int> ___horizontalSlotsPerGroup)
 		{
