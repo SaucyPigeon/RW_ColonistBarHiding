b9256621f26c296fd57c6e30751d802e1b0658ab
SaucyPigeon <36158286+SaucyPigeon@users.noreply.github.com>
2020-01-21 09:46:53 +0000

    Code cleanup
    More comments, deeper folder structure for patches, ColonistBarOnGUI() patch no longer uses instance as parameter.

---
+++ Source/ColonistBarHiding/ColonistBarHiding/Patches/ColonistBarDrawLocsFinder/ColonistBarDrawLocsFinder_CalculateGroupsCount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using Verse;
+using RimWorld;
+
+namespace ColonistBarHiding.Patches.ColonistBarDrawLocsFinder
+{
+	using ColonistBarDrawLocsFinder = RimWorld.ColonistBarDrawLocsFinder;
+
+	/// <summary>
+	/// Patch for ColonstBarDrawLocsFinder.CalculateGroupsCount(). This patch
+	/// takes hidden entries into account.
+	/// </summary>
+	[HarmonyPatch(typeof(ColonistBarDrawLocsFinder))]
+	[HarmonyPatch("CalculateGroupsCount")]
+	internal class ColonistBarDrawLocsFinder_CalculateGroupsCount
+	{
+		[HarmonyPrefix]
+		private static bool Prefix(int __result)
+		{
+			__result = ColonistBarUtility.CalculateGroupsCount();
+			return false;
+		}
+	}
+}
